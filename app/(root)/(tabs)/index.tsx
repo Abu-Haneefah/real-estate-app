685e3a4156a13770f01ef98ee38ae141c80e73ce
Jabar Fatai <aabdfatahi@gmail.com>
2026-01-23 10:40:12 +0100

    set up the app enviroment

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,24 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-2xl text-red-300 font-bold italic my-10">
+        Welcome to RealState
+      </Text>
+      <View className="my-5 flex flex-col gap-4 text-neutral-400">
+        <Link href="/sign-in">Sign In</Link>
+        <Link href="/explore">Explore</Link>
+        <Link href="/profile">Profile</Link>
+        <Link href="/properties/[id]">Property</Link>
+      </View>
+    </View>
+  );
+}
